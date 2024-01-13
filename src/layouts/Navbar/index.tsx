776f406a7d94ee1ff0d633de6dae3741f2f83649
Nice <hoangkimc93@gmail.com>
2024-01-14 05:47:29 +0700

    chore(UI): add navbar

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -6,15 +6,29 @@ import React from "react";
 const NavBar: React.FC = () => {
   return (
     <div className="flex flex-col min-h-[100vh]">
-      <div className="px-12 bg-neutral-3 h-20 text-white flex flex-row items-center justify-between sticky top-0 left-0 z-10">
+      <div className="px-12 bg-gray-800 h-20 text-white flex flex-row items-center justify-between sticky top-0 left-0 z-20">
         <Link to={path.HOMEPAGE} className="text-heading-4">
-          LOGO
+          CỜ GÁNH
         </Link>
+        <div className="flex flex-row items-center h-full">
+          <Link
+            to={path.HOMEPAGE}
+            className="flex justify-center items-center text-heading-4 hover:bg-gray-500 h-full"
+          >
+            <span className="p-4 text-body-1-semibold">Cách chơi</span>
+          </Link>
+          <Link
+            to={path.HOMEPAGE}
+            className="flex justify-center items-center text-heading-4 hover:bg-gray-500 h-full"
+          >
+            <span className="p-4 text-body-1-semibold">Liên hệ</span>
+          </Link>
+        </div>
       </div>
       <div className=" min-h-[calc(100vh-160px)] h-fit max-w-7xl w-11/12 mx-auto my-4">
         <Outlet />
       </div>
-      <div className="px-12 bg-neutral-3 h-12 w-full text-white flex flex-row items-center justify-center">
+      <div className="px-12  bg-gray-800 h-12 w-full text-white flex flex-row items-center justify-center">
         <span className="text-heading-5">FOOTER</span>
       </div>
     </div>
--- src/pages/PlayChess/index.tsx
+++ src/pages/PlayChess/index.tsx
@@ -1,27 +1,49 @@
 import BoardTile from "@/pages/PlayChess/components/BoardTile";
 import { useChessBoard } from "@/pages/PlayChess/hooks/useChessBoard";
+import { Avatar, Card } from "antd";
+import Meta from "antd/es/card/Meta";
 import React from "react";
 
 const PlayChess: React.FC = () => {
   const { chessBoard, moveChessPiece, pickUpChess, currentPlayer } =
     useChessBoard();
   return (
-    <div className="flex flex-col  max-h-[calc(100vh-160px)] w-auto aspect-square m-auto">
-      {chessBoard.map((row, rowIndex) => (
-        <div className="max-w-[90vw] flex flex-row" key={rowIndex}>
-          {row.map((chessPiece, col) => (
-            <BoardTile
-              chessPiece={chessPiece}
-              col={col}
-              row={rowIndex}
-              key={`${row}.${col}`}
-              pickUpChess={pickUpChess}
-              moveChessPiece={moveChessPiece}
-              currentPlayer={currentPlayer}
-            />
-          ))}
-        </div>
-      ))}
+    <div className=" relative flex lg:flex-row flex-col max-h-[calc(100vh-160px)] w-auto aspect-square m-auto">
+      <Card className="  lg:absolute  lg:-right-64 lg:top-20 relative">
+        <Meta
+          avatar={
+            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          }
+          title="Máy"
+          description="Chơi quân đỏ"
+        />
+      </Card>
+      <div className="flex flex-col  max-h-[calc(100vh-160px)] w-full  m-auto">
+        {chessBoard.map((row, rowIndex) => (
+          <div className="max-w-[90vw] flex flex-row" key={rowIndex}>
+            {row.map((chessPiece, col) => (
+              <BoardTile
+                chessPiece={chessPiece}
+                col={col}
+                row={rowIndex}
+                key={`${row}.${col}`}
+                pickUpChess={pickUpChess}
+                moveChessPiece={moveChessPiece}
+                currentPlayer={currentPlayer}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <Card className="  lg:absolute  lg:-right-64 lg:bottom-20 relative">
+        <Meta
+          avatar={
+            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+          }
+          title="Bạn"
+          description="Chơi quân xanh"
+        />
+      </Card>
     </div>
   );
 };
